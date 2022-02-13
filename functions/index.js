0eafa8dfeeb3c9ae34ef88932d447a6d8a32ed81
Caroline Del Carmen <delcarmencaroline@gmail.com>
2022-02-13 16:49:23 -0500

    added 2 endpoitns

--- functions/index.js
+++ functions/index.js
@@ -14,14 +14,54 @@ const cors = require("cors");
 const app = express();
 app.use(cors({ origin: true }));
 
+// Main databse reference
+const db = admin.firestore();
+
 // Routes
 app.get("/", (req, res) => {
-    return res.status(200).send(Success);
+    return res.status(200).send("Success");
 });
 
 //Create - post()
- 
-//Get - get()
+app.post("/api/create", (req, res) => {
+    (async () => {
+        try{
+            await db.collection('Patients').doc("COVID-19-AR-16406" + "EXAMPLE_CREATE").create({
+                bmi : req.body.bmi,
+                dob : req.body.dob,
+                firstName : req.body.firstName,
+                height : req.body.height,
+                icuAdmit : req.body.icuAdmit,
+                lastName :  req.body.lastName,
+                race : req.body.race,
+                sex : req.body.sex,
+                weight : req.body.weight,
+                zip : req.body.zip
+            });
+            return res.status(200).send({ status : 'Success', msg : 'Data Saved' });
+        } catch(error) {
+            console.log(error);
+            return res.status(500).send({ status : 'Failed', msg : error });
+        }
+    })();
+}); 
+
+// Get - get()
+// Fetch - Single Data form firestore using specific ID
+app.get('/api/get/:id', (req, res) => {
+    (async () => {
+        try {
+            const reqDoc = db.collection('Patients').doc(req.params.id);
+            let patient = await reqDoc.get();
+            let response = patient.data();
+
+            return res.status(200).send({ status: "Success", data: response });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({ status: "Failed", msg: error });
+        }
+    })();
+});
 
 //Update - put()
 
